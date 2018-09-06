7bbcc681d27404d6b5354e628ad9ec806dd27181
ksweet <ksweet@fischerhomes.com>
2018-09-06 11:37:13 -0400

    Removed Test Code since it is no longer needed

--- DIScheduler/Scheduler.cs
+++ DIScheduler/Scheduler.cs
@@ -1,21 +1,12 @@
-﻿using DIScheduler.Core.Interfaces;
-using DIScheduler.Interfaces;
+﻿using DIScheduler.Interfaces;
 
 namespace DIScheduler
 {
     public class Scheduler : IScheduler
     {
-        private readonly ISapphireService _sapphireService;
-
-        public Scheduler(ISapphireService sapphireService)
-        {
-            _sapphireService = sapphireService;
-        }
-
         public void PollSapphireQueue()
         {
-            //Go Check the Last Run Table to see if we have a valid last run date
-            var nextRunList = _sapphireService.ListNextRuns();
+            //Go Check the Last Run Table to see if we have a valid last run date            
         }
     }
 }
