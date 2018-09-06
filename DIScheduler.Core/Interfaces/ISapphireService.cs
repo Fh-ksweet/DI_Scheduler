e8dd51d986f600c74fc69126a304eabda4b27026
ksweet <ksweet@fischerhomes.com>
2018-09-06 11:37:44 -0400

    Removed more test code

--- DIScheduler.Core/Interfaces/ISapphireService.cs
+++ DIScheduler.Core/Interfaces/ISapphireService.cs
@@ -1,10 +1,6 @@
-﻿using DIScheduler.Core.Model;
-using System.Collections.Generic;
-
-namespace DIScheduler.Core.Interfaces
+﻿namespace DIScheduler.Core.Interfaces
 {
     public interface ISapphireService
     {
-        IList<NextRun> ListNextRuns();
     }
 }
--- DIScheduler.Core/Services/SapphireService.cs
+++ DIScheduler.Core/Services/SapphireService.cs
@@ -1,21 +1,8 @@
 ﻿using DIScheduler.Core.Interfaces;
-using DIScheduler.Core.Model;
-using System.Collections.Generic;
 
 namespace DIScheduler.Core.Services
 {
     public class SapphireService : ISapphireService
     {
-        private readonly INextRunRepository _nextRunRepository;
-
-        public SapphireService(INextRunRepository nextRunRepository)
-        {
-            _nextRunRepository = nextRunRepository;
-        }
-
-        public IList<NextRun> ListNextRuns()
-        {
-            return _nextRunRepository.ListNextRuns();
-        }
     }
 }
